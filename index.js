77cd64c6095888f295f6d8d72c7a5dfda4297966
belyanin <belyanin@skbkontur.ru>
2021-12-31 00:20:00 +0500

    Working prototype

--- index.js
+++ index.js
@@ -10,51 +10,189 @@ const spriteSize = 64;
 
 canvas.addEventListener('mousedown', handleOnClick);
 canvas.addEventListener('mousemove', handleMove);
-canvas.addEventListener('mouseleave', () => selectedItem = null);
-canvas.addEventListener('mouseup', () => selectedItem = null);
+canvas.addEventListener('mouseleave', handleMouseLeave);
+canvas.addEventListener('mouseup', handleMouseLeave);
 
 const canvasRect = canvas.getBoundingClientRect();
 const ctx = canvas.getContext("2d");
 
 setInterval(draw, 40)
 
-let items = [
+const itemToTitle = {
+    "forest": "Лес",
+    "negative": "Негатив",
+    "work": "Работа",
+    "money": "Деньги",
+    "water": "Влага",
+    "wind": "Ветер",
+    "tester": "Тестировщик",
+    "pivko": "Пив&ко",
+    "anton": "Антоша",
+    "ants": "Муравьи",
+    "cigarettes": "Сигареты",
+    "dm": "Дядя Миша",
+    "dm_dead": "Мертвый Дядя Миша",
+    "dm_dead_eight": "Восемь мертвых Дядей Миш",
+    "dm_dead_four": "Четвыре мертвых Дяди Мишей",
+    "dm_dead_six": "Шесть мертвых Дядей Миш",
+    "dm_dead_two": "Два метрвых Дяди Миши",
+    "dm_dressed": "Одетый Дядя Миша",
+    "dm_wet": "Влажный Дядя Миша",
+    "persimmon": "Хурма",
+    "persimmon_with_ants": "Хурма с муравьями",
+    "portal": "Портвл в Ад"
+}
+
+const recipies = [
     {
-        x: 100,
-        y: 100,
-        id: "forest",
-        title: "Лес"
+        first: "negative",
+        second: "work",
+        result: "money"
     },
     {
-        x: 200,
-        y: 150,
-        id: "money",
-        title: "Деньги"
+        first: "wind",
+        second: "money",
+        result: "tester"
+    },
+    {
+        first: "money",
+        second: "water",
+        result: "pivko"
+    },
+    {
+        first: "dm",
+        second: "water",
+        result: "dm_wet"
+    },
+    {
+        first: "dm_wet",
+        second: "wind",
+        result: "dm_dead4"
+    },
+    {
+        first: "tester",
+        second: "work",
+        result: "persimmon"
+    },
+    {
+        first: "tester",
+        second: "forest",
+        result: "ants"
+    },
+    {
+        first: "persimmon",
+        second: "ants",
+        result: "persimmon_with_ants"
+    },
+    {
+        first: "work",
+        second: "wind",
+        result: "forest"
+    },
+    {
+        first: "dm_wet",
+        second: "wind",
+        result: "dm_dead4"
+    },
+    {
+        first: "cigarettes",
+        second: "tester",
+        result: "anton"
+    },
+    {
+        first: "negative",
+        second: "tester",
+        result: "dm"
+    },
+    {
+        first: "ants",
+        second: "persimmon",
+        result: "persimmon_with_ants"
+    },
+    {
+        first: "money",
+        second: "pivko",
+        result: "cigarettes"
+    },
+    {
+        first: "dm_dead_six",
+        second: "dm_dead_four",
+        result: "portal"
+    },
+    {
+        first: "dm_dead_eight",
+        second: "dm_dead_two",
+        result: "portal"
+    },
+    {
+        first: "dm_dead",
+        second: "dm_dead",
+        result: "dm_dead_two"
+    },
+    {
+        first: "dm_dead_two",
+        second: "dm_dead_two",
+        result: "dm_dead_four"
+    },
+    {
+        first: "dm_dead_two",
+        second: "dm_dead_four",
+        result: "dm_dead_six"
+    },
+    {
+        first: "dm_dead_six",
+        second: "dm_dead_two",
+        result: "dm_dead_eight"
+    },
+    {
+        first: "dm_dead_four",
+        second: "dm_dead_four",
+        result: "dm_dead_eight"
     }
 ];
 
 const textures = {};
 
-for (let item of items) {
+for (let item in itemToTitle) {
 
     const image = new Image();
-    image.src = `./images/${item.id}.png`
+    image.src = `./images/${item}.png`
 
-    textures[item.id] = image;
+    textures[item] = image;
 }
 
 let field = [
     {
-        x: 100,
-        y: 100,
-        id: "forest",
-        title: "Лес"
+        x: 200,
+        y: 400,
+        id: "water",
+        isEternal: true,
+        startX: 200,
+        startY: 400
     },
     {
-        x: 200,
-        y: 150,
-        id: "money",
-        title: "Деньги"
+        x: 300,
+        y: 400,
+        id: "wind",
+        isEternal: true,
+        startX: 300,
+        startY: 400
+    },
+    {
+        x: 400,
+        y: 400,
+        id: "negative",
+        isEternal: true,
+        startX: 400,
+        startY: 400
+    },
+    {
+        x: 500,
+        y: 400,
+        id: "work",
+        isEternal: true,
+        startX: 500,
+        startY: 400
     }
 ]
 
@@ -65,7 +203,7 @@ function draw() {
     ctx.fillStyle = 'black';
     for (let item of field) {
         ctx.drawImage(textures[item.id], item.x, item.y);
-        ctx.fillText(item.title, item.x, item.y + spriteSize + 12);
+        ctx.fillText(itemToTitle[item.id], item.x, item.y + spriteSize + 12);
     }
 }
 
@@ -78,6 +216,53 @@ function handleMove(event) {
     const mouse = getMousePosition(event);
     selectedItem.x = mouse.x - spriteSize / 2;
     selectedItem.y = mouse.y - spriteSize / 2;
+}
+
+function handleMouseLeave(event) {
+    if (!selectedItem)
+        return;
+
+    craft_search: for (let item of field) {
+        if (item === selectedItem)
+            continue;
+
+        if (item.x + spriteSize <= selectedItem.x || selectedItem.x + spriteSize < item.x ||
+            item.y + spriteSize < selectedItem.y || selectedItem.y + spriteSize < item.y)
+            continue;
+
+        console.log(`Found intersection with ${item.id}`)
+
+        for (let recipe of recipies) {
+            if (recipe.first === selectedItem.id && recipe.second === item.id ||
+                recipe.second === selectedItem.id && recipe.first === item.id) {
+
+                field = field.filter(i => i.isEternal || (i !== item && i !== selectedItem));
+
+                if (item.isEternal) {
+                    field.push({
+                        x: item.x,
+                        y: item.y - spriteSize - 16,
+                        id: recipe.result
+                    })
+                }
+                else {
+                    field.push({
+                        x: selectedItem.x,
+                        y: selectedItem.y,
+                        id: recipe.result
+                    })
+                }
+
+                break craft_search;
+            }
+        }
+    }
+
+    if (selectedItem.isEternal) {
+        selectedItem.x = selectedItem.startX;
+        selectedItem.y = selectedItem.startY;
+    }
+    selectedItem = null;
 }
 
 function handleOnClick(event) {
